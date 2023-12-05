5c2851541b6ddc8883fd57ee798d79a0ea50ed30
Lastman89 <alexeygr89@gmail.com>
2023-12-05 19:02:18 +0300

    without comments

--- src/Main.java
+++ src/Main.java
@@ -6,7 +6,7 @@ import java.util.stream.Stream;
 
 public class Main {
     public static void main(String[] args) {
-        List<Integer> intList = Arrays.asList(1, 2, 5, 16, -1, -2, 0, 32, 3, 5, 8, 23, 4); //Почему нельзя было взять обычный массив?!
+        List<Integer> intList = Arrays.asList(1, 2, 5, 16, -1, -2, 0, 32, 3, 5, 8, 23, 4);
         System.out.println(intList);
         noStream(intList);
         withStream(intList);
