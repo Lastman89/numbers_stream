cd95dbb0ffdc688ff4131396c8a78106f19d3fd7
Lastman89 <alexeygr89@gmail.com>
2023-12-05 19:01:08 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,53 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+import java.util.stream.Stream;
+
+public class Main {
+    public static void main(String[] args) {
+        List<Integer> intList = Arrays.asList(1, 2, 5, 16, -1, -2, 0, 32, 3, 5, 8, 23, 4); //Почему нельзя было взять обычный массив?!
+        System.out.println(intList);
+        noStream(intList);
+        withStream(intList);
+
+
+    }
+
+    public static void noStream(List numbers) {
+        List<Integer> intList = new ArrayList<>();
+        for (int i = 0; i < numbers.size(); i++) {
+            if ((int) numbers.get(i) > 0 && (int) numbers.get(i) % 2 == 0) {
+                intList.add((int) numbers.get(i));
+            } else {
+                continue;
+            }
+        }
+        int[] list = new int[intList.size()];
+        for (int i = 0; i < intList.size(); i++) {
+            list[i] = (int) intList.get(i);
+        }
+        for (int i = 0; i < list.length - 1; i++) {
+            for (int j = 0; j < list.length - i - 1; j++) {
+                if (list[j] > list[j + 1]) {
+                    int temp = list[j + 1];
+                    list[j + 1] = list[j];
+                    list[j] = temp;
+                }
+            }
+
+        }
+        System.out.println(Arrays.toString(list));
+
+    }
+
+    public static void withStream(List numbers) {
+        Stream<Integer> stream = numbers.stream()
+                .filter(x -> (int) x > 0)
+                .filter(x -> (int) x % 2 == 0)
+                .sorted(Comparator.naturalOrder());
+        Object[] list = stream.toArray();
+        System.out.println(Arrays.toString(list));
+    }
+
+}
